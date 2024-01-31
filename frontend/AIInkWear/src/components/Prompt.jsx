282b4b656722fdac624aecc73c21cd8a9fbfb4f0
rdmiller00 <rd.miller0619@gmail.com>
2024-01-30 19:50:37 -0500

    initial picture size and display set

--- frontend/AIInkWear/src/components/Prompt.jsx
+++ frontend/AIInkWear/src/components/Prompt.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
-import ImageSplitter from "./ImageSplitter";
+
 
 
 const Prompt = () => {
   const [prompt, setPrompt] = useState("");
   const [generatedImage, setGeneratedImage] = useState("");
-  
 
   function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
@@ -24,38 +23,39 @@ const Prompt = () => {
     axios(config)
       .then(async function (response) {
         console.log(response.data);
-        setGeneratedImage(JSON.stringify(response.data));
-        let finished = false;
-        while(!finished){
-          let picture = await axios.get(`http://localhost:3000/message/` + response.data.messageId)
-          console.log(picture.data);
-          picture.data.uri ? finished = true : false
-          console.log("Loading image...")
-          if(!finished){
-          await sleep(30000)
-          }
-        }
-        console.log(picture.data.uri);
+        setGeneratedImage(response.data.uri);
+        checkProgressAndFetchImage(response.data.messageId);
       })
       .catch(function (error) {
         console.log(error);
       });
   };
+
+        const checkProgressAndFetchImage = async (messageId) => {
+    const picture = await axios.get(`http://localhost:3000/message/${messageId}`);
+    if (picture.data.progress !== 100) {
+      console.log("Loading image...");
+      await sleep(10000);
+      checkProgressAndFetchImage(messageId);
+    } else {
+      console.log("Image loaded:", picture.data.uri);
+      setGeneratedImage(picture.data.uri);
+    }
+  };
+
   return (
-      <div>
-     <ImageSplitter />
     <div>
       <input
         className="promptInput"
-        placeholder="T-Shirt......Transparent background"
+        placeholder="Enter prompt here! Have fun!"
         value={prompt}
         onChange={(e) => setPrompt(e.target.value)}
       />
       <button className="generateButton" onClick={handleGenerateImage}>
         Generate Image
       </button>
-      <div>{generatedImage}</div>
-    </div>
+      <br />
+      {generatedImage && <img src={generatedImage} alt="Generated Image" className="newImage" />}
     </div>
   );
 };
