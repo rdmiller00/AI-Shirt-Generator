5e18b8ebf5058c1846c5435819d9ecc93a182e70
rdmiller00 <rd.miller0619@gmail.com>
2024-01-30 20:33:54 -0500

    pictures displaying, styling ongoing, progress message displaying while image loads

--- frontend/AIInkWear/src/components/Prompt.jsx
+++ frontend/AIInkWear/src/components/Prompt.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 const Prompt = () => {
   const [prompt, setPrompt] = useState("");
   const [generatedImage, setGeneratedImage] = useState("");
+  const [loadingMessage, setLoadingMessage] = useState("");
 
   function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
@@ -35,11 +36,13 @@ const Prompt = () => {
     const picture = await axios.get(`http://localhost:3000/message/${messageId}`);
     if (picture.data.progress !== 100) {
       console.log("Loading image...");
-      await sleep(10000);
+      setLoadingMessage(`Like human art, generative AI art takes time too. Your design is about ${picture.data.progress || 0}% done`);
+      await sleep(5000);
       checkProgressAndFetchImage(messageId);
     } else {
       console.log("Image loaded:", picture.data.uri);
       setGeneratedImage(picture.data.uri);
+      setLoadingMessage("");
     }
   };
 
@@ -54,6 +57,8 @@ const Prompt = () => {
       <button className="generateButton" onClick={handleGenerateImage}>
         Generate Image
       </button>
+      <br />
+      <h2>{loadingMessage}</h2>
       <br />
       {generatedImage && <img src={generatedImage} alt="Generated Image" className="newImage" />}
     </div>
